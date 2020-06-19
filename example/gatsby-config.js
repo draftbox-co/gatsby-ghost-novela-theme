e17fa45c8a5985fbe2cd55d82069a13b06700acd
Shyam <shyamlohar1996@gmail.com>
2020-06-19 22:29:02 +0530

    changed data source for header and footer links and added facebook comments and removed console logs and other minor changes and improvements

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -19,6 +19,59 @@ module.exports = {
           backgroundColor: `#e9e9e9`, // Used for Offline Manifest
           themeColor: `#15171A`, // Used for Offline Manifest
           apiUrl: "https://ghost.theasdfghjkl.com",
+          header: {
+            navigation: [
+              {
+                label: "Home",
+                url: "https://ghost-balsa-preview.draftbox.co/",
+              },
+              {
+                label: "Contact",
+                url: "https://ghost-balsa-preview.draftbox.co/contact",
+              },
+            ],
+          },
+          footer: {
+            copyright: "Built with Draftbox122",
+            navigation: [
+              {
+                label: "Home",
+                url: "https://ghost-balsa-preview.draftbox.co/",
+              },
+              {
+                label: "Sitemap",
+                url: "https://ghost-balsa-preview.draftbox.co/sitemap.xml",
+              },
+              {
+                label: "RSS",
+                url: "https://ghost-balsa-preview.draftbox.co/rss",
+              },
+              {
+                label: "Contact",
+                url: "https://ghost-balsa-preview.draftbox.co/contact",
+              },
+              {
+                label: "External Link",
+                url: "https://spectrum.chat/gatsby-js/themes?tab=posts",
+              },
+            ],
+          },
+          subscribeWidget: {
+            title: "Subscribe to Draftbox [new]",
+            helpText: "Get the latest posts delivered right to your inbox.",
+            successMessage: "Thanks for subscribing to Built with Draftbox.",
+          },
+          socialLinks: {
+            twitter: "https://twitter.com/draftboxhq",
+            facebook: "https://facebook.com/",
+            instagram: "https://www.instagram.com/",
+            linkedin: "https://linkedin.com",
+            github: "https://github.com/draftbox-co",
+          },
+          contactWidget: {
+            title: "Contact Built with Draftbox",
+            successMessage: "We’ll get in touch with you soon.",
+          },
         },
         ghostConfig: {
           development: {
@@ -32,15 +85,6 @@ module.exports = {
             version: "v3",
           },
         },
-      },
-    },
-    {
-      resolve: `gatsby-source-ghost`,
-      options: {
-        apiUrl: `https://ghost.theasdfghjkl.com`,
-        contentApiKey: `3d17fad3efaa911df1ed577638`,
-        version: `v3`, // Ghost API version, optional, defaults to "v3".
-        // Pass in "v2" if your Ghost install is not on 3.0 yet!!!
       },
     },
   ],
