c8b26f72fe0eb67559b3653a728603843bae4247
Keyur <keyur.raval18@gmail.com>
2020-07-13 15:54:44 +0530

    Adds contact meta support

--- gatsby-ghost-novela-theme/src/pages/contact.tsx
+++ gatsby-ghost-novela-theme/src/pages/contact.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
 import Layout from "../components/Layout";
+import ContactMeta from "@components/meta/ContactMeta";
 
-const Contact = () => {
+const Contact = ({ location }) => {
   return (
     <Layout>
+      <ContactMeta location={location} />
       <ContactForm />
     </Layout>
   );
